95780ce4acc273ad5f25749cf8076414c3999164
MIMi <mimijohngrey@outlook.com>
2024-08-27 16:25:39 +0100

    card

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card({ children }) {
   return (
-    <div className="border border-purple-400 text-center rounded-lg shadow-xl pb-4 mx-auto">
+    <div className="border border-purple-400 text-center rounded-lg shadow-xl pb-4 mx-auto w-full lg:w-[350px]">
       {children}
     </div>
   );
