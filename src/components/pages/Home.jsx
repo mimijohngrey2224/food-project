4439b0f20348d916e5fa6d00bfbe6d4a2f408ec0
MIMi <mimijohngrey@outlook.com>
2024-08-27 22:24:57 +0100

    new

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,6 +7,9 @@ import waitress from "/waitress.jpg";
 import waiter from "/waiter.jpg";
 import cocktail2 from "/cocktail2.jpg";
 import redwine from "/redwine.jpg";
+import hilda from "/hilda.jpeg";
+import foodin from "/foodin.jpeg";
+import chefa from "/chefa.jpg";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 function Home() {
@@ -115,7 +118,31 @@ function Home() {
                 alt="Redwine Image"
                 className="h-[100px] sm:h-[200px] md:h-[250px] lg:h-[300px] w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] object-cover rounded-full shadow-md transition-transform duration-300 hover:scale-105"
               />
-              <p>Red wine range in hue from <br /> deep, opaque purple to pale <br /> ruby and everything in between</p>
+              <p className="mt-2 text-center">Red wine range in hue from <br /> deep, opaque purple to pale <br /> ruby and everything in between</p>
+            </div>
+            <div className="flex flex-col items-center">
+            <img
+                src={hilda}
+                alt="Hilda Image"
+                className="h-[100px] sm:h-[200px] md:h-[250px] lg:h-[300px] w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] object-cover rounded-full shadow-md transition-transform duration-300 hover:scale-105"
+              />
+              <p className="mt-2 text-center">A Nigerian chef Hilda Bassey, <br /> a Guinness World Record holder <br /> for the longest cooking marathon GWR</p>
+            </div>
+            <div className="flex flex-col items-center">
+            <img
+                src={foodin}
+                alt="Foodin Image"
+                className="h-[100px] sm:h-[200px] md:h-[250px] lg:h-[300px] w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] object-cover rounded-full shadow-md transition-transform duration-300 hover:scale-105"
+              />
+              <p className="mt-2 text-center">The arrangement of <br /> food on a plate</p>
+            </div>
+            <div className="flex flex-col items-center">
+            <img
+                src={chefa}
+                alt="Chefa Image"
+                className="h-[100px] sm:h-[200px] md:h-[250px] lg:h-[300px] w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] object-cover rounded-full shadow-md transition-transform duration-300 hover:scale-105"
+              />
+              <p className="mt-2 text-center">A chef is that person with <br /> multitasking and creativity, <br /> also paying attention <br /> to customers orders</p>
             </div>
           </div>
         </div>
