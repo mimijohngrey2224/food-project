e8d0fe276866f8288ca636a47d4a4cb361b4c72d
MIMi <mimijohngrey@outlook.com>
2024-08-27 11:54:07 +0100

    ok

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -75,7 +75,9 @@ function Home() {
         alt="Cocktail Image"
         className="h-[100px] sm:h-[200px] md:h-[250px] lg:h-[300px] w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] object-cover rounded-full shadow-md transition-transform duration-300 hover:scale-105"
       />
-      <p className="mt-2 text-center">List some ingredients and <br /> give an idea of the flavour, <br /> adding emphasis on any <br /> elements that elevate the cocktail</p>
+      <div className="mt-2 text-center w-10">
+      <p >List some ingredients and give an idea of the flavour, adding emphasis on any elements that elevate the cocktail</p>
+      </div>
     </div>
     <div className="flex flex-col items-center">
       <img
